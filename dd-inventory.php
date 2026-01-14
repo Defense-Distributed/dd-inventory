a23f6261ad4d6ad69689de292a4a7fb32924b733
Bloblems <self@bloblems.com>
2026-01-13 20:04:01 -0600

    Bump version to 1.0.1 for update test

--- dd-inventory.php
+++ dd-inventory.php
@@ -3,7 +3,7 @@
  * Plugin Name: DD Inventory
  * Plugin URI: https://github.com/Defense-Distributed/dd-inventory
  * Description: Syncs WooCommerce inventory with an external inventory management system. The inventory tracker pushes products/inventory to WooCommerce, and WooCommerce sends order webhooks back.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Defense Distributed
  * Author URI: https://defcad.com
  * License: GPL v2 or later
@@ -19,7 +19,7 @@
 defined('ABSPATH') || exit;
 
 // Define plugin constants
-define('DDI_VERSION', '1.0.0');
+define('DDI_VERSION', '1.0.1');
 define('DDI_PLUGIN_FILE', __FILE__);
 define('DDI_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('DDI_PLUGIN_URL', plugin_dir_url(__FILE__));
